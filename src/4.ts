1fb9f91e0a7ec2b5b5da68f2623684697c4217aa
YuriiKniaz <yuri.kniaz@gmail.com>
2023-11-18 17:07:13 +0200

    TypeScript HW 3

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,54 @@
-const key = new Key();
+class Key {
+
+    constructor(private signature: number) {
+        this.signature = Math.floor(Math.random());
+    }
+    
+    getSignature():number {
+        return this.signature;
+    }
+}
+
+class Person {
+    constructor(private key: Key) {
+    }
+    
+    getKey(): Key{
+        return this.key;
+    }
+}
+
+abstract class House{
+    door: boolean = false;
+    tenants: Person[] = [];
+
+    constructor(protected key: Key) {
+       
+    }
+
+    comeIn(person: Person): void {
+        if (!this.door) {
+            this.tenants.push(person)
+        }
+    }
+
+    abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House{
+    openDoor(key: Key): void {
+        if (key === this.key) {
+            this.door = true;
+            
+        }
+        else {
+            
+        }
+    }
+}
+
+
+const key = new Key(1337);
 
 const house = new MyHouse(key);
 const person = new Person(key);
@@ -8,4 +58,25 @@ house.openDoor(person.getKey());
 house.comeIn(person);
 
 
-export {};
+export { };
+
+
+// У цьому завдання вам належить реалізувати сценарій життя, де людина, ключ і будинок взаємодіють один з одним.
+
+// Ключ (Key): Створіть клас Key. У нього має бути одна приватна властивість signature, яка генерується випадково при створенні об'єкта цього класу (наприклад Math.random()).
+// Також цей клас повинен мати метод getSignature, який повертає значення властивості signature.
+
+// Людина (Person): Створіть клас Person. Конструктор цього класу приймає об'єкт класу Key і зберігає їх у приватному властивості key. 
+    
+// Клас Person повинен мати метод getKey, який повертає збережений ключ.
+
+// Дім (House): Створіть абстрактний клас House. Цей клас має дві властивості: door, яка може бути відкрита (true), або закрита (false), і key, яка зберігає об'єкт класу Key.
+//  У цьому класі також повинен бути метод comeIn, який додає об'єкт класу Person у масив tenants, якщо door відкрита. 
+//  Ваш абстрактний клас House також повинен мати абстрактний метод OpenDoor, який приймає об'єкт класу Key.
+
+// Мій будинок (MyHouse): Створіть клас MyHouse, який успадковується від абстрактного класу House. Реалізуйте метод openDoor у цьому класі. 
+// Якщо ключ, переданий цьому методу, збігається з ключем, збереженим як key, то двері відчиняються.
+
+// Після реалізації всіх класів створіть об'єкти для кожного класу та спробуйте відтворити сценарій, в якому людина приходить додому.
+
+// Наприклад, ось так:
